0318d0eb7725317ce8ae1901fa088ddcaea9df59
Killian Debacker <43973287+Killian264@users.noreply.github.com>
2019-04-17 13:41:53 -0500

    Add files via upload

---
+++ KillianBot/CommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Discord.Commands;
+using Discord.WebSocket;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace KillianBot
+{
+    public class CommandHandler
+    {
+       private DiscordSocketClient _client;
+
+       private CommandService _service;
+
+       char cmdLetter = '!';
+
+       public CommandHandler(DiscordSocketClient client)
+        {
+            _client = client;
+
+            _service = new CommandService();
+
+            _service.AddModulesAsync(Assembly.GetEntryAssembly(), null);
+
+            _client.MessageReceived += HandleCommandAsync;
+
+
+        }
+
+        private async Task HandleCommandAsync(SocketMessage s)
+        {
+            var msg = s as SocketUserMessage;
+            if(msg == null) return;
+
+            var context = new SocketCommandContext(_client, msg);
+
+            int argPos = 0;
+            if (msg.HasCharPrefix(cmdLetter, ref argPos))
+            {
+                    var result = await _service.ExecuteAsync(context, argPos, null);
+
+                    if (!result.IsSuccess && result.Error != CommandError.UnknownCommand)
+                    {
+                        await context.Channel.SendMessageAsync(result.ErrorReason);
+                    }
+            }
+        }
+    }
+}
---
+++ KillianBot/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Discord;
+using Discord.WebSocket;
+using System.Threading.Tasks;
+
+namespace KillianBot
+{
+    public class Program
+    {
+        const string _clientID = "ID";
+
+        static void Main(string[] args)
+        => new Program().StartAsync().GetAwaiter().GetResult();
+
+        private DiscordSocketClient _client;
+
+        private CommandHandler _handler;
+
+        public async Task StartAsync()
+        {
+            _client = new DiscordSocketClient();
+
+            await _client.LoginAsync(TokenType.Bot, _clientID);
+
+            await _client.StartAsync();
+
+            _handler = new CommandHandler(_client);
+
+            await Task.Delay(-1);
+
+        }
+    }
+}
