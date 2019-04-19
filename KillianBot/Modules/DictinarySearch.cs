6843b408ad88b9995a768aee4746aba2e7dda021
Killian Debacker <43973287+Killian264@users.noreply.github.com>
2019-04-19 15:26:12 -0500

    Improved error catch and improved output

--- KillianBot/Modules/DictinarySearch.cs
+++ KillianBot/Modules/DictinarySearch.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System;
 using Discord.Commands;
 using System.Threading.Tasks;
-using System.IO;
-using Discord.WebSocket;
-using KillianBot.Services;
-using Newtonsoft.Json;
 using System.Linq;
 using Discord;
 
@@ -14,43 +8,36 @@ namespace KillianBot.Modules
 {
     public class Define : ModuleBase<SocketCommandContext>
     {
-        string MerriamBase = "https://www.merriam-webster.com/dictionary/";
-
         [Discord.Commands.Command("define")]
-        public async Task define(string word)
+        public async Task Definition(string word)
         {
-            if (String.IsNullOrEmpty(word)) return;
-            var results = await Services.KillianBotService.DefinitionGet.GetDef(word);
-            if (results.word != word) await ReplyAsync("No results"); 
-            else
-            {
-                var embed = new EmbedBuilder()
-                        .WithAuthor(word.First().ToString().ToUpper() + word.Substring(1) + "---Definition")
-                        .WithColor(Color.Blue)
-                        .WithFooter(MerriamBase + word);
+            string MerriamBase = "https://www.merriam-webster.com/dictionary/";
+            Services.KillianBotService.DictionaryList results = new Services.KillianBotService.DictionaryList();
 
-                if(results.meaning.noun != null)
-                {
-                    embed.AddField("Noun", results.meaning.noun[0].definition);
-                }
-                if (results.meaning.verb != null)
-                {
-                    embed.AddField("Verb", results.meaning.verb[0].definition);
-                }
-                if (results.meaning.adverb != null)
-                {
-                    embed.AddField("Adverb", results.meaning.adverb[0].definition);
-                }
-                if (results.meaning.adjective != null)
-                {
-                    embed.AddField("Adjective", results.meaning.adjective[0].definition);
-                }
-                if (results.meaning.exclamation != null)
-                {
-                    embed.AddField("Exclamation", results.meaning.exclamation[0].definition);
-                }
-                await ReplyAsync(embed: embed.Build());
+            if (String.IsNullOrEmpty(word)) return;
+            try
+            {
+                results = await Services.KillianBotService.DefinitionGet.GetDef(word);
             }
+            catch
+            {
+                await ReplyAsync("No results");
+            }
+
+            var embed = new EmbedBuilder()
+                .WithAuthor(word.First().ToString().ToUpper() + word.Substring(1) + "---Definition")
+                .WithTitle("Definition Link")
+                .WithUrl("https://www.google.com/search?rlz=1C1CHBF_enUS715US715&ei=KCq6XOvFAsOisAXRuJPYBQ&q=" + word + "+def&oq=dammit+def&gs_l=psy-ab.3..0i67i70i249j0l7j0i22i30l2.5814.9270..9323...8.0..0.157.2131.0j17....2..0....1..gws-wiz.....6..35i39j0i131j0i67j0i10j0i10i67j0i131i67.vr1ivIEsmVQ")
+                .WithColor(Color.DarkerGrey)
+                .WithFooter(MerriamBase + word);
+
+            if (results.meaning.noun != null) embed.AddField("Noun", results.meaning.noun[0].definition);
+            if (results.meaning.verb != null) embed.AddField("Verb", results.meaning.verb[0].definition);
+            if (results.meaning.adverb != null) embed.AddField("Adverb", results.meaning.adverb[0].definition);
+            if (results.meaning.adjective != null) embed.AddField("Adjective", results.meaning.adjective[0].definition);
+            if (results.meaning.exclamation != null) embed.AddField("Exclamation", results.meaning.exclamation[0].definition);
+
+            await ReplyAsync(embed: embed.Build());
         }
     }
     
