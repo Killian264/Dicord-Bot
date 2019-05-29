55a6a943b4776b44b6f9c1b4e07e20b619490860
Killian Debacker <43973287+Killian264@users.noreply.github.com>
2019-05-29 00:26:00 -0500

    Add files via upload

--- KillianBot/Services/Collections.cs
+++ KillianBot/Services/Collections.cs
@@ -4,11 +4,86 @@ using System.Text;
 using Newtonsoft.Json;
 using KillianBot.Services;
 using Reddit;
+using Discord.Net;
+using Discord;
 
 namespace KillianBot.Services
 {
     class Collections
     {
+        public static Dictionary<string, Settings> commands = new Dictionary<string, Settings>
+        {
+                {"Birthday", new Settings{
+                    permission = GuildPermission.SendMessages,
+                    aliases = new string[1] {"bday"},
+                    description = "Checks birthdays and sends next birthday or checks if someones birthday is today.",
+                    howUse = "!birthday"} },
+
+                {"BirthdayAll", new Settings{
+                    permission = GuildPermission.SendMessages,
+                    aliases = new string[3] {"birthday all", "bday all", "bdayall"},
+                    description = "Gets all birthdays from file",
+                    howUse = "!birthdayAll" } },
+
+                {"Define", new Settings{
+                    permission = GuildPermission.SendMessages,
+                    aliases = new string[2] {"definition", "def"},
+                    description = "Defines word",
+                    howUse = "!define word" } },
+
+                {"DefineAll", new Settings{
+                    permission = GuildPermission.SendMessages,
+                    aliases = new string[5] {"define all", "definition all", "definitionall", "def all", "defall"},
+                    description = "Gets all definitons of word",
+                    howUse = "!defineAll word" } },
+
+                {"RedditImages", new Settings{
+                    permission = GuildPermission.SendMessages,
+                    aliases = new string[4] {"ImagesReddit", "RedditImgGet", "Reddit Images", "Get Image Reddit"},
+                    description = "Sends Images from a reddit of your choice",
+                    howUse = "!redditImages subreddit howmanyImgs" } },
+
+                {"Help", new Settings{
+                    permission = GuildPermission.SendMessages,
+                    aliases = new string[1] {"None"},
+                    description = "Helps",
+                    howUse = "!help or !help commandname" } }
+        };
+        public class myAnimeListData
+        {
+            public int mal_id { get; set; }
+            public string url { get; set; }
+            public string image_url { get; set; }
+            public string title { get; set; }
+            public bool airing { get; set; }
+            public string synopsis { get; set; }
+            public string type { get; set; }
+            public int episodes { get; set; }
+            public double score { get; set; }
+            public int members { get; set; }
+            public string rated { get; set; }
+        }
+
+        public class myAnimeList
+        {
+            public string request_hash { get; set; }
+            public bool request_cached { get; set; }
+            public int request_cache_expiry { get; set; }
+            [JsonProperty("results")]
+            public List<myAnimeListData> data { get; set; }
+            public int last_page { get; set; }
+        }
+        public class Settings
+        {
+            public GuildPermission permission { get; set; }
+
+            public string[] aliases { get; set; }
+
+            public string description { get; set; }
+
+            public string howUse { get; set; }
+        }
+
         public class DictionaryList
         {
             public string word { get; set; }
@@ -92,11 +167,15 @@ namespace KillianBot.Services
             public string RedditAppSecret { get; set; }
 
             public string RedditAppRefreshToken { get; set; }
+
+            public AliasesOfEach aliases { get; set; }
         }
 
         public static class Config
         {
             public static ConfigList configList = new ConfigList();
+            
+            public static AliasesOfEach allAliases { get; set; }
         }
 
         public static class Dict
@@ -107,6 +186,16 @@ namespace KillianBot.Services
         {
             public DateTime created { get; set; }
             public List<Reddit.Controllers.Post> posts { get; set; }
+        }
+        public class AliasesOfEach
+        {
+            public string[] birthday { get; set; }
+
+            public string[] birthdayAll { get; set; }
+
+            public string[] define { get; set; }
+            public string[] definAll { get; set; }
+            public string[] redditImages { get; set; }
         }
     }
 
